d399f4dc70767ae273c6e3a968a9c0c007bb5bd1
Andres <a.galea.2022@alumnos.urjc.es>
2026-06-14 22:13:58 +0200

    Update depth_2_cloud function and add function to export depth images

--- utils.py
+++ utils.py
@@ -5,6 +5,8 @@ from cv_bridge import CvBridge
 import os
 from sensor_msgs_py import point_cloud2
 
+bridge = CvBridge()
+
 def load_intrinsics_from_yaml(path):
     with open(path, "r") as f:
         data = yaml.safe_load(f)
@@ -12,7 +14,7 @@ def load_intrinsics_from_yaml(path):
     K = np.array(data["camera"]["K"], dtype=np.float32).reshape(3, 3)
     return K
 
-def depth_2_cloud(depth_img, K, organized=False):
+def depth_2_cloud(depth_img, K, scale=False, organized=False):
     if K is None:
         return
 
@@ -24,11 +26,21 @@ def depth_2_cloud(depth_img, K, organized=False):
 
     H, W = depth.shape
 
-    magic_num = 3
+    # magic_num = 3
+    # magic_num = 640 / 1920
+    if scale:
+        scale_x = 1920/640
+        scale_y = 1080/480
+    else:
+        scale_x = 1
+        scale_y = 1
 
     # Escala de la imagen de color con la imagen derecha 1920/640
-    fx, fy = K[0, 0]*magic_num, K[1, 1]*magic_num
-    cx, cy = K[0, 2]*magic_num, K[1, 2]*magic_num
+    # fx, fy = K[0, 0]*scale_x, K[1, 1]*scale_x
+    # cx, cy = K[0, 2]*scale_y, K[1, 2]*scale_y
+
+    fx, fy = K[0, 0]*scale_x, K[1, 1]*scale_y
+    cx, cy = K[0, 2]*scale_x, K[1, 2]*scale_y
 
     u, v = np.meshgrid(np.arange(W), np.arange(H))
 
@@ -70,20 +82,20 @@ def export_cloud(msg, out_dir):
     return
 
 def export_cloud_from_depth(rosdepth, k, outdir, organized=False):
-    bridge = CvBridge()
+    #bridge = CvBridge()
 
     ts = rosdepth.header.stamp.sec * 1_000_000_000 + rosdepth.header.stamp.nanosec
     filename = os.path.join(outdir, f"{ts}.bin")
 
     depth_img = bridge.imgmsg_to_cv2(rosdepth, desired_encoding="passthrough")
 
-    depth_cloud = depth_2_cloud(depth_img, k, organized)
+    depth_cloud = depth_2_cloud(depth_img, k, True, organized)
     depth_cloud.astype(np.float32).tofile(filename)
 
     return
 
 def export_image(ros_msg, out_dir):
-    bridge = CvBridge()
+    #bridge = CvBridge()
     cv_image = bridge.imgmsg_to_cv2(ros_msg, desired_encoding='bgr8')
 
     ts = ros_msg.header.stamp.sec * 1_000_000_000 + ros_msg.header.stamp.nanosec
@@ -92,3 +104,27 @@ def export_image(ros_msg, out_dir):
     cv2.imwrite(filename, cv_image)
 
     return
+
+def export_depth_image(msg, out_dir):
+    depth_img = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
+
+    if depth_img.dtype == np.uint16:
+        depth_in_mm = depth_img
+
+    else:
+        depth_in_mm = depth_img.astype(np.float32) * 1000.0
+
+        depth_in_mm = np.nan_to_num(
+            depth_in_mm,
+            nan=0.0,
+            posinf=0.0,
+            neginf=0.0,
+        )
+
+        depth_in_mm = np.clip(depth_in_mm, 0, 65535)
+        depth_in_mm = depth_in_mm.astype(np.uint16)
+
+    ts = msg.header.stamp.sec * 1_000_000_000 + msg.header.stamp.nanosec
+    out_img_path = os.path.join(out_dir, f"{ts}.png")
+
+    cv2.imwrite(out_img_path, depth_in_mm)
