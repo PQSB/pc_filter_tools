5cadc1e33aee4b2a0f0bb527568794da7f022125
Andres <a.galea.2022@alumnos.urjc.es>
2026-07-15 02:21:41 +0200

    Improve load_intrinsics_from_yaml

--- 3D_MOOD/mood_utils.py
+++ 3D_MOOD/mood_utils.py
@@ -1,18 +1,18 @@
 import numpy as np
 import yaml
-import cv2
-# from cv_bridge import CvBridge
-import os
-# from sensor_msgs_py import point_cloud2
-
-# bridge = CvBridge()
 
 def load_intrinsics_from_yaml(path):
-    with open(path, "r") as f:
-        data = yaml.safe_load(f)
+    try:
+        with open(path, "r") as f:
+            data = yaml.safe_load(f)
 
-    K = np.array(data["camera"]["K"], dtype=np.float32).reshape(3, 3)
-    return K
+        K = np.array(data["camera"]["K"], dtype=np.float32).reshape(3, 3)
+        return K
+
+    except Exception as e:
+        print(f"[ERROR] Failed to load matrix K '{path}'. Details: {e}")
+
+    return None
 
 def depth_2_cloud(depth_img, K, scale=False, organized=False):
     if K is None:
@@ -62,72 +62,6 @@ def depth_2_cloud(depth_img, K, scale=False, organized=False):
         cloud = np.column_stack((xlidar[mask], ylidar[mask], zlidar[mask]))
 
     return cloud
-
-# def pointcloud2_to_xyz(msg):
-#     points = list(point_cloud2.read_points_numpy(
-#     msg,
-#     # field_names=("x", "y", "z", "intensity"),
-#     field_names=("x", "y", "z"),
-#     skip_nans=True
-#     ))
-#     return np.array(points, dtype=np.float32)
-
-# def export_cloud(msg, out_dir):
-#     ts = msg.header.stamp.sec * 1_000_000_000 + msg.header.stamp.nanosec
-#     filename = os.path.join(out_dir, f"{ts}.bin")
-
-#     cloud = pointcloud2_to_xyz(msg)
-#     cloud.astype(np.float32).tofile(filename)
-
-#     return
-
-# def export_cloud_from_depth(rosdepth, k, outdir, organized=False):
-#     #bridge = CvBridge()
-
-#     ts = rosdepth.header.stamp.sec * 1_000_000_000 + rosdepth.header.stamp.nanosec
-#     filename = os.path.join(outdir, f"{ts}.bin")
-
-#     depth_img = bridge.imgmsg_to_cv2(rosdepth, desired_encoding="passthrough")
-
-#     depth_cloud = depth_2_cloud(depth_img, k, True, organized)
-#     depth_cloud.astype(np.float32).tofile(filename)
-
-#     return
-
-# def export_image(ros_msg, out_dir):
-#     #bridge = CvBridge()
-#     cv_image = bridge.imgmsg_to_cv2(ros_msg, desired_encoding='bgr8')
-
-#     ts = ros_msg.header.stamp.sec * 1_000_000_000 + ros_msg.header.stamp.nanosec
-
-#     filename = os.path.join(out_dir, f"{ts}.png")
-#     cv2.imwrite(filename, cv_image)
-
-#     return
-
-# def export_depth_image(msg, out_dir):
-#     depth_img = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
-
-#     if depth_img.dtype == np.uint16:
-#         depth_in_mm = depth_img
-
-#     else:
-#         depth_in_mm = depth_img.astype(np.float32) * 1000.0
-
-#         depth_in_mm = np.nan_to_num(
-#             depth_in_mm,
-#             nan=0.0,
-#             posinf=0.0,
-#             neginf=0.0,
-#         )
-
-#         depth_in_mm = np.clip(depth_in_mm, 0, 65535)
-#         depth_in_mm = depth_in_mm.astype(np.uint16)
-
-#     ts = msg.header.stamp.sec * 1_000_000_000 + msg.header.stamp.nanosec
-#     out_img_path = os.path.join(out_dir, f"{ts}.png")
-
-#     cv2.imwrite(out_img_path, depth_in_mm)
 
 def load_kitti_matrix(calib_file, prefix):
     if not prefix.endswith(':'):
